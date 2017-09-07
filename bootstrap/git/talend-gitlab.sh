156fed1f9eb9439bf7d513518c1f6fa7d8f14cdc
EdwardOst <edward.ost@gmail.com>
2017-09-06 22:48:21 -0400

    updated gitlab bootstrap scripts

---
+++ bootstrap/git/talend-gitlab.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -eu
+
+function gitlab_init() {
+
+    local usage="gitlab_init.sh <git_admin_userid> <git_admin_password> <git_admin_email> <git_tac_userid> <git_tac_password> <git_tac_email> <git_repo> <source_url>"
+
+    local git_admin_userid="${1:-${git_admin_userid:-}}"
+    local git_admin_password="${2:-${git_admin_password:-}}"
+    local git_admin_email="${3:-${git_admin_email:-}}"
+    local git_tac_userid="${4:-${git_tac_userid:-}}"
+    local git_tac_password="${5:-${git_tac_password:-}}"
+    local git_tac_email="${6:-${git_tac_email:-}}"
+    local git_repo="${7:-${git_repo:-}}"
+    local source_url="${8:-${source_url:-}}"
+
+    [ -z "${git_admin_userid}"] && echo "invalid argument git_admin_userid cannot be empty: usage: ${usage}" && return 1
+    [ -z "${git_admin_password"] && echo "invalid argument git_admin_password cannot be empty: usage: ${usage}" && return 1
+    [ -z "${git_admin_email"] && echo "invalid argument git_admin_email cannot be empty: usage: ${usage}" && return 1
+    [ -z "${git_tac_userid}"] && echo "invalid argument git_tac_userid cannot be empty: usage: ${usage}" && return 1
+    [ -z "${git_tac_password}"] && echo "invalid argument git_tac_password cannot be empty: usage: ${usage}" && return 1
+    [ -z "${git_tac_email"] && echo "invalid argument git_tac_email cannot be empty: usage: ${usage}" && return 1
+    [ -z "${git_repo}"] && echo "invalid argument git_repo cannot be empty: usage: ${usage}" && return 1
+    [ -z "${source_url}"] && echo "invalid argument source_url cannot be empty: usage: ${usage}" && return 1
+
+    #Install the packages.  The wait is to prevent a stall after reconfigure
+
+    # gitlab-ctl reconfigure
+    # sleep 1m
+    # apt-get install -y ruby
+    # apt-get install -y jq
+    # gem install gitlab
+
+    # Configure git accounts with usernames/passwords
+
+    # need to confirm that the gitlab-rails runner will accept unquoted arguments.  the outer quote needs to use double quotes in order
+    # for bash parameter expansion to work
+    gitlab-rails runner "User.create!(username: ${git_admin_userid}, email: test2@example.com, password: ${git_admin_password}, password_confirmation: ${git_admin_password}, name: ${git_admin_userid}, admin: true)"
+
+    gitlab-rails runner "User.create!(username: ${git_tac_userid}, email: test@example.com, password: ${git_tac_password}, password_confirmation: ${git_tac_password}, name: ${git_tac_userid})"
+
+    # This next section puts the ip in as the first variable and the access token as the second
+
+    local ipvar=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+
+    local xvar=$(curl http://${ipvar}/api/v3/session --data "login=${git_admin_userid}&password=${git_admin_password}" | jq --raw-output .private_token)
+
+    # Create the project, clone it to local file, populate it with demo, and push it
+
+    curl --header "PRIVATE-TOKEN: ${xvar}" -X POST "http://${ipvar}/api/v3/projects?name=${git_repo}"
+
+    git clone "http://${git_admin_userid}:${git_admin_password}@${ipvar}/${git_admin_userid}/oodlejobs.git"
+
+    cd "${git_repo}"
+
+    # placeholder to retrieve standard jobs
+    wget "${source_url}"
+
+    git add .
+    git commit -m "First Jobs"
+    git push http://Admin:AdminPassword@${ipvar}/Admin/oodlejobs.git
+
+}
