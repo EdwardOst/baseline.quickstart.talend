c5b1ccc63d77bac0b0ccfd199b44f246eac9fd9a
EdwardOst <edward.ost@gmail.com>
2017-09-19 02:23:41 -0400

    updated studio ubuntu template to set ubuntu user rather than ec2-user default for s3fs mount

--- scripts/factory/talend/factory-env.sh
+++ scripts/factory/talend/factory-env.sh
@@ -19,6 +19,7 @@ function factory_env() {
     local talend_userid="${talend_userid:-${TALEND_FACTORY_TALEND_USERID:-}}"
     local talend_password="${talend_password:-${TALEND_FACTORY_TALEND_PASSWORD:-}}"
     local s3fs_url="${s3fs_url:-${TALEND_FACTORY_S3FS_URL:-https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.82.tar.gz}}"
+    local default_user="${default_user:-${TALEND_FACTORY_DEFAULT_USER:-}}"
 
     required access_key secret_key talend_userid talend_password
 
